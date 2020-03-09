7196ab92ac62d6d0eecac55dad12275781e9c374
BrZrKr43 <faizdragonvalli@gmail.com>
2020-03-09 14:53:24 +0200

    pythoon file

---
+++ NEW PYTHON FILE.py
@@ -0,0 +1,22 @@
+print("Hello IDE")
+
+#adding fraction
+a=3
+b=2
+c=1
+
+numerator=a-3
+dennominator=b*b+c*c
+
+print(numerator/dennominator)
+
+#print(numerator/ddennominator)
+#print(a/3=c/b)
+
+import math
+a=3
+b=2
+a=math.sqrt(c*c+b*b)
+print(x)
+
+
