b3a4b9cc22c695558922d93004f2e36bce4753a5
BrZrKr43 <faizdragonvalli@gmail.com>
2020-03-10 16:13:25 +0200

    exercise

---
+++ file 1.py
@@ -0,0 +1,19 @@
+print("Hello IDE")
+
+# Adding fractions
+a=4
+b=3
+c=5
+d=10
+numerator=a*d+b*c
+denominator=b*d
+
+print(numerator/denominator)
+
+#print(numerator/denominator)
+#print(a/b=c/d)
+import math
+a=4
+b=3
+c=math.sqrt(a*a+b*b)
+print(c)
