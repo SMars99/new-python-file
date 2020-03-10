32f480648344a0622deab7677366fc0ff4ab0ad8
BrZrKr43 <faizdragonvalli@gmail.com>
2020-03-10 16:09:02 +0200

    add

---
+++ lifechoices.py
@@ -0,0 +1,32 @@
+strSentence="Lifechoices Coding Academy"
+print(type(strSentence))
+print(strSentence[2:])
+Number1=25
+Number2=50.5
+Number3=3e+7j
+print(type(Number1),type(Number2),type(Number3))
+#this declares a list called mylist
+
+mylist= [65,25,125,78,120,33]
+mylist.sort()
+print(mylist)
+mylist.insert(2,37)
+print(mylist)
+mylist.reverse()
+print(mylist)
+
+cars=("Datsun","Ford","BMW","VW")
+print(type(cars))
+
+list=[23,48,1,"godwin"]
+print(type(list))
+
+xdict={"1":"Amy","2":"Tania","3":"Nathan"}
+print(type(xdict))
+
+
+
+
+
+
+
